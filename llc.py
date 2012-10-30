961cddfcddbec8a999b0b68e228c430dad8bbcfb
Ricky Chang <ricchang@ebay.com>
2012-10-30 18:07:16 -0400

    fixing print_url offset handling

--- llc.py
+++ llc.py
@@ -1,22 +1,39 @@
 import mechanize
+import urlnorm
+import urllib2
+import re
 import sys
+
+VALID_CHARS = '^([-A-Za-z0-9+&@#/%=~_()| ]*)$'
+
+def validate_search_term(search_term):
+	return bool(re.search(VALID_CHARS, search_term))
+
+def print_url(url, letter_index, max_index):
+	if (not url or letter_index == -1):
+		print ''
+	else:
+		padding = ' ' * (max_index - letter_index)
+		print '%s%s %s %s' % (padding, url[0:letter_index], url[letter_index], url[letter_index+1:])
 
 def find_matching_links(br, target_word, result, visited):
 	if (not target_word):
 		return result
 	else:
-		letter = target_word[0]
-		if (letter.isspace()):
+		current_URL = br.geturl()
+		current_letter = target_word[0]
+		if (current_letter.isspace()):
 			return find_matching_links(br, target_word[1:], result + [('', -1, ' ')], visited)
 		else:
-			matches = [(link, link.absolute_url[7:].find(letter.lower()) + 7) for link in br.links() if link.absolute_url[7:].find(letter.lower())!= -1]
+			matches = [(link, link.absolute_url[7:].find(current_letter.lower()) + 7) for link in br.links() if link.absolute_url[7:].find(current_letter.lower())!= -1]
 			for m in matches:
 				try:
+					# if (urlnorm.norm(m[0].absolute_url) not in visited):
 					if (m[0].absolute_url not in visited):
-						visited.add(m[0].absolute_url)
+						visited.add(urlnorm.norm(m[0].absolute_url))
 						print m[0].absolute_url
 						br.follow_link(m[0])
-						child_result = find_matching_links(br, target_word[1:], result + [(m[0].absolute_url, m[1], letter)], visited)
+						child_result = find_matching_links(br, target_word[1:], result + [(m[0].absolute_url, m[1], current_letter)], visited)
 						if (child_result):
 							return child_result
 						else:
@@ -25,12 +42,7 @@ def find_matching_links(br, target_word, result, visited):
 					pass
 	return []
 
-def print_url(url, letter_index, display_width):
-	if (not url or letter_index == -1):
-		print ''
-	else:
-		padding = ' ' * ((display_width / 2) - letter_index)
-		print '%s%s %s %s' % (padding, url[0:letter_index], url[letter_index], url[letter_index+1:])
+
 
 
 if (len(sys.argv) < 3 ):
@@ -39,22 +51,32 @@ if (len(sys.argv) < 3 ):
 	exit(0)
 root_URL = sys.argv[1]
 search_term = sys.argv[2]
+if (not validate_search_term(search_term)):
+	print "Invalid search term.  Please only use valid url characters and spaces."
+	exit(1)
+
 first_letter = search_term[0]
 first_letter_match = root_URL.find(first_letter.lower())
 if (first_letter_match != -1):
-	br = mechanize.Browser()
-	result = [(root_URL, first_letter_match, first_letter)]
-	visited = set([root_URL])
-	print root_URL
 	try:
+		br = mechanize.Browser()
+		result = [(root_URL, first_letter_match, first_letter)]
+		visited = set([urlnorm.norm(root_URL)])
 		br.open(root_URL)
+		print root_URL
 		result = find_matching_links(br, search_term[1:], result, visited)
 		if (result):
 			print result
-			max_length = len(max(result, key=lambda u: len(u[0]))[0])
+			max_index = max(result, key=lambda u: u[1])[1]
 			for l, i, c in result:
-				print_url(l, i, max_length * 2)
-	except Exception as e:
+				print_url(l, i, max_index)
+	except urlnorm.InvalidUrl:
+		print "Invalid root URL"
+	except urllib2.URLError:
+		print "Error opening root URL"
+	except Exception, e:
+		print e
+	finally:
 		exit(1)
 
 
